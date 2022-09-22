f73bfe60a2b959c7f96d568e18439224f7018e64
Chris Bradfield <cb@scribe.net>
2022-09-22 19:49:21 +0000

    updates

---
+++ dist_code/no-vowels.c
@@ -0,0 +1,10 @@
+// Write a function to replace vowels with numbers
+// a -> 6, e -> 3, i -> 1, o -> 0
+// ./no-vowels word
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+
+}
